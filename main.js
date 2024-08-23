a1c2e47bead07efb39a106c4bd4a8c78b5d220b9
Darkbrotherhood <bogatovs93@gmail.com>
2024-08-23 12:09:24 +0500

    5 edits

--- main.js
+++ main.js
@@ -1,33 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
+ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector(".head-container").classList.toggle("open")
-    })
-});
+    });
 
-document.getElementById('menu').addEventListener('click', event => {
-    event._isClickWithInMenu = true;
-});
-document.getElementById('burger').addEventListener('click', event => {
-    event._isClickWithInMenu = true;
-});
-document.body.addEventListener('click', event => {
-    if (event._isClickWithInMenu) return;
-    document.querySelector('.head-container').classList.remove('open')
-});
+    document.getElementById('menu').addEventListener('click', event => {
+        event._isClickWithInMenu = true;
+    });
+    document.getElementById('burger').addEventListener('click', event => {
+        event._isClickWithInMenu = true;
+    });
 
-document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener('click', event => {
+        if (event._isClickWithInMenu) return;
+        document.querySelector('.head-container').classList.remove('open')
+    });
+
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector(".blackout").classList.toggle("blackout_open")
-    })
-});
+    });
 
-document.getElementById('menu').addEventListener('click', event => {
-    event._isClickWithInMenu = true;
+    document.getElementById('menu').addEventListener('click', event => {
+        event._isClickWithInMenu = true;
+    });
+    document.getElementById('burger').addEventListener('click', event => {
+        event._isClickWithInMenu = true;
+    });
+    document.body.addEventListener('click', event => {
+        if (event._isClickWithInMenu) return;
+        document.querySelector('.blackout').classList.remove('blackout_open')
+    });
+
+    document.getElementById("search").addEventListener("click", function() {
+        document.querySelector(".header_search_block").classList.toggle("is_active")
+    });
+
+    document.getElementById("search_close").addEventListener("click", function() {
+        document.querySelector(".header_search_block").classList.remove("is_active")
+    });
+
+    const search_el = document.querySelectorAll('#search, #search_close');
+    search_el.forEach((element) => {
+    element.addEventListener("click", function() {
+        if(window.innerWidth <= 677) {
+            document.querySelector(".main_logo_link").classList.toggle("hidden")
+        } else {
+            document.querySelector(".main_logo_link").classList.remove("hidden")
+        }
+    });
 });
-document.getElementById('burger').addEventListener('click', event => {
-    event._isClickWithInMenu = true;
-});
-document.body.addEventListener('click', event => {
-    if (event._isClickWithInMenu) return;
-    document.querySelector('.blackout').classList.remove('blackout_open')
-});
+})
+
+
+
+
+
